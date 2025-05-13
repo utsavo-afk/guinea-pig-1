88c5f4d76e79cd03b2aec4d538efcaf6ada36591
Utsav Ojha <utsav.ojha@hotmail.com>
2025-05-13 14:55:42 +0530

    style(page.tsx): update heading text to personalize the homepage and enhance user experience

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-24">
-      <h1 className="text-6xl font-bold underline underline-offset-[12px]">Howdy NextJs Flyio PNPM</h1>
+      <h1 className="text-6xl font-bold underline underline-offset-[12px]">My NextJs Flyio PNPM</h1>
       <Button size={"lg"} className="mt-6 px-12 py-4">Deploy Me</Button>
     </div>
   );
